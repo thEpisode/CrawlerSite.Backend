04bb50b1c82dbe3053bf81248b3ff111dd4cabd6
Cami Rodriguez <camiepisode@outlook.com>
2016-11-30 16:08:24 -0500

    getting image via socket

--- app/controllers/socketController.js
+++ app/controllers/socketController.js
@@ -1,48 +1,69 @@
-function Socket(dependencies){
+function Socket(dependencies) {
 
     /// Dependencies
     var _console;
     var _io;
     var _database;
+    var _fileHandler;
 
-    var constructor = function(){
-        _io         = dependencies.io;
-        _database   = dependencies.database;
-        _console        = dependencies.console;
+    var constructor = function () {
+        _io = dependencies.io;
+        _database = dependencies.database;
+        _console = dependencies.console;
+        _fileHandler = dependencies.fileHandler;
 
         socketImplementation();
         _console.log('Socket module initialized', 'server-success');
     }
 
-    var socketImplementation = function(){
-        _io.sockets.on('connection', function(socket){
+    var socketImplementation = function () {
+        _io.sockets.on('connection', function (socket) {
             _console.log('Client connected: ' + socket.id, 'socket-message');
 
             /// Welcome to the new client
-            socket.emit('Welcome', {Message: 'Welcome to Coplest.Flinger', SocketId : socket.id});
+            socket.emit('Welcome', { Message: 'Welcome to Coplest.Flinger', SocketId: socket.id });
 
             /// Request all insights queue
-            socket.emit('Coplest.Flinger.ServerEvent', {Command : 'InsightsQueue'});
+            socket.emit('Coplest.Flinger.ServerEvent', { Command: 'InsightsQueue' });
 
-            socket.on('Coplest.Flinger.PushInsight', function(data){
-                if (data.Command != undefined){
-                    switch(data.Command){
+            socket.on('Coplest.Flinger.PushInsight', function (data) {
+                if (data.Command != undefined) {
+                    switch (data.Command) {
                         case 'Click':
-                            _database.Click().CreateClick(data.Values, function(){
+                            _database.Click().CreateClick(data.Values, function () {
                                 //console.log('Click Saved');
                             })
                             break;
                         case 'Movement':
-                            _database.Movement().CreateMovement(data.Values, function(){
+                            _database.Movement().CreateMovement(data.Values, function () {
                                 //console.log('Movement Saved');
                             })
                             break;
                         case 'Scroll':
-                            _database.Scroll().CreateScroll(data.Values, function(){
+                            _database.Scroll().CreateScroll(data.Values, function () {
                                 //console.log('Scroll Saved');
                             })
                             break;
                     }
+                }
+            })
+
+            socket.on('Coplest.Flinger.PushScreenshot', function (data) {
+                if (data.Command != undefined) {
+                    _database.Site().WebCrawling(Values.ApiKey, Values.Endpoint, function (result) {
+                        // if hasn't a screenshot
+                        if (result == false) {
+                            _fileHandler.CreateScreenshotFile(Values.Base64Data, Values.Endpoint, function (result) {
+                                // Add new image to entity
+                                _database.Site().AddScreenshotToChild(Values.ApiKey, result._id + '', Values.Endpoint, function () {
+                                    res.json({ message: 'CreateScreenshotFile', result: true });
+                                })
+                            })
+                        }
+                        else {
+                            res.json({ message: 'CreateScreenshotFile', result: true });
+                        }
+                    })
                 }
             })
         });
