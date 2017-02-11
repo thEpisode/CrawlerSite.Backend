172297c1f25d56127586801a7ab793e371bd010a
Cami Rodriguez <camiepisode@outlook.com>
2017-02-11 10:29:56 -0500

    added model

---
+++ app/Models/FrontEndReview.js
@@ -0,0 +1,49 @@
+function FrontEndReview(dependencies) {
+
+    /// Dependencies
+    var _mongoose;
+    var _schema;
+
+    /// Properties
+    var _model;
+    var _states;
+
+    var constructor = function () {
+        _mongoose = dependencies.mongoose;
+        _schema = _mongoose.Schema;
+
+        _states = {
+            Unread: 0,
+            Readed: 1,
+        }
+
+        _model = _mongoose.model('FrontEndReview', new _schema(
+            {
+                UserId: String,
+                Path: String,
+                Like: Boolean,
+                ScreenshotId: String,
+                Description: String,
+                Logs: String,
+                ReportBug : Boolean
+            },
+            { timestamps: { createdAt: 'created_at' }, minimize: false, collection: 'Ip' }
+        ));
+    }
+
+    var getModel = function () {
+        return _model;
+    }
+
+    var getStates = function () {
+        return _states;
+    }
+
+    return {
+        Initialize: constructor,
+        GetModel: getModel,
+        GetStates: getStates
+    }
+}
+
+module.exports = FrontEndReview;
