8852d833720a92791a3e5b5b54ed2c09bf5e8b3b
Cami Rodriguez <camiepisode@outlook.com>
2017-05-04 11:40:16 -0500

    Added new models

---
+++ app/Models/CreditCard.js
@@ -0,0 +1,47 @@
+function Subscription(dependencies) {
+
+    /// Dependencies
+    var _mongoose;
+    var _schema;
+
+    /// Properties
+    var _model;
+    var _states;
+
+    var constructor = function () {
+        _mongoose = dependencies.mongoose;
+        _schema = _mongoose.Schema;
+
+        _states = {
+            Inactive: 0,
+            Active: 1
+        }
+
+        _model = _mongoose.model('Subscription', new _schema(
+            {
+                _id: _schema.Types.ObjectId,
+                CreditCardToken: String,
+                FirstNameCard: String,
+                LastNameCard: String,
+                State: Number
+            },
+            { timestamps: { createdAt: 'created_at' }, minimize: false, collection: 'Subscription' }
+        ));
+    }
+
+    var getModel = function () {
+        return _model;
+    }
+
+    var getStates = function () {
+        return _states;
+    }
+
+    return {
+        Initialize: constructor,
+        GetModel: getModel,
+        GetStates: getStates
+    }
+}
+
+module.exports = Subscription;
