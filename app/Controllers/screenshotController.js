e2a03bee67ef24f02d80ea547871d4a3534bf5df
Cami Rodriguez <camiepisode@outlook.com>
2017-07-07 11:05:37 -0500

    added new controller

---
+++ app/Controllers/screenshotController.js
@@ -0,0 +1,116 @@
+function ScreenshotController(dependencies) {
+
+    /// Dependencies  
+    var _console;
+    var _mongoose;
+
+    /// Properties
+    var _entity;
+
+    var constructor = function () {
+        _mongoose = dependencies.mongoose;
+        _console = dependencies.console;
+
+        _entity = require('../Models/Screenshot')(dependencies);
+        _entity.Initialize();
+    }
+
+    var createScreenshot = function (data, callback) {
+
+        var screenshot = new _entity.GetModel()(
+            {
+                Timestamp: data.Timestamp,
+                Screenshot: data.Screenshot,
+                Endpoint: data.Endpoint,
+                Type: data.Type,
+                ApiKey: data.ApiKey,
+                IsObsolete: false,
+            });
+
+        screenshot.save().then(function (result) {
+            // When database return a result call the return
+            callback({ success: true, message: 'CreateScreenshot', result: result });
+        }, function (err) {
+            _console.log(err, 'error');
+            callback({ success: false, message: 'Something was wrong while creating screenshot', result: null });
+        })
+    }
+
+    var deleteScreenshotById = function (data, callback) {
+        _entity.GetModel().findOneAndRemove(data.Id, function (err, result) {
+            if (err) {
+                _console.log(err, 'error');
+                callback({ success: false, message: 'Something was wrong while deleting screenshot', result: null });
+            }
+            else {
+                callback({ success: true, message: 'deleteScreenshot', result: result });
+            }
+        })
+    }
+
+    var getScreenshotById = function (data, callback) {
+        _entity.GetModel().findOne({ "_id": data.Id }, function (err, result) {
+            if (err) {
+                _console.log(err, 'error');
+                callback({ success: false, message: 'Something was wrong while getting screenshot', result: null });
+            }
+            else {
+                callback({ success: true, message: 'getScreenshotById', result: result });
+            }
+
+        })
+    }
+
+    var getScreenshotByApiKey = function (data, callback) {
+        _entity.GetModel().findOne({ "ApiKey": data.ApiKey }, function (err, result) {
+            if (err) {
+                _console.log(err, 'error');
+                callback({ success: false, message: 'Something was wrong while getting screenshot', result: null });
+            }
+            else {
+                callback({ success: true, message: 'getScreenshotByApiKey', result: result });
+            }
+        })
+    }
+
+    var getAllScreenshot = function (data, callback) {
+        _entity.GetModel().find({}, function (err, result) {
+            if (err) {
+                _console.log(err, 'error');
+                callback({ success: false, message: 'Something was wrong while getting screenshot', result: null });
+            }
+            else {
+                callback({ success: true, message: 'getScreenshotByApiKey', result: result });
+            }
+        })
+    }
+
+    var getIfLastScreenshotIsObsoleteByApiKey = function () {
+        _entity.GetModel().findOne({ "ApiKey": data.ApiKey }, {}, { sort: { 'created_at': -1 } }, function (err, result) {
+            if (err) {
+                _console.log(err, 'error');
+                callback({ success: false, message: 'Something was wrong while getting screenshot', result: null });
+            }
+            else {
+                callback({ success: true, message: 'getScreenshotByApiKey', result: result });
+            }
+        })
+    }
+
+    var getEntity = function () {
+        return _entity;
+    }
+
+    return {
+        Initialize: constructor,
+        CreateScreenshot: createScreenshot,
+        DeleteScreenshotById: deleteScreenshotById,
+        GetScreenshotById: getScreenshotById,
+        GetScreenshotByApiKey: getScreenshotByApiKey,
+        GetAllScreenshot: getAllScreenshot,
+        GetIfLastScreenshotIsObsoleteByApiKey: getIfLastScreenshotIsObsoleteByApiKey,
+        Entity: getEntity
+    }
+}
+
+module.exports = ScreenshotController;
---
+++ app/Models/Screenshot.js
@@ -0,0 +1,44 @@
+function Screenshot(dependencies) {
+
+    /// Dependencies
+    var _mongoose;
+    var _schema;
+
+    /// Properties
+    var _model;
+    var _screenshotType;
+
+    var constructor = function () {
+        _mongoose = dependencies.mongoose;
+        _schema = _mongoose.Schema;
+
+        _screenshotType = {
+            seen: 0,
+            allPage: 1,
+            partial: 2
+        }
+
+        _model = _mongoose.model('Screenshot', new _schema(
+            {
+                Timestamp: String,
+                Screenshot: Buffer,
+                Endpoint: String,
+                Type: Number,
+                ApiKey: String,
+                IsObsolete: Boolean,
+            },
+            { timestamps: { createdAt: 'created_at' }, minimize: false, collection: 'Screenshot' }
+        ));
+    }
+
+    var getModel = function () {
+        return _model;
+    }
+
+    return {
+        Initialize: constructor,
+        GetModel: getModel
+    }
+}
+
+module.exports = Screenshot;
